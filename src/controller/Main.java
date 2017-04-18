e97066358cc7d11467adedf1a48f8dbbaa6bbb80
alex205 <alexis.girardi@linux.com>
2017-04-18 07:16:46 +0200

    plus de gens (#alone)

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,11 +11,11 @@ public class Main {
     public static void main(String[] args) throws UnknownHostException {
         Controller controller = new Controller(); //instantiation du contrôleur
 
-        ContactCollection cc = ContactCollection.getInstance();
+        /*ContactCollection cc = ContactCollection.getInstance();
         cc.addContact(new Contact("alex205", InetAddress.getByName("127.0.0.1")));
         cc.addContact(new Contact("Patrick974", InetAddress.getByName("127.0.0.1")));
         cc.addContact(new Contact("Toonu", InetAddress.getByName("127.0.0.1")));
-        cc.addContact(new Contact("Drosik", InetAddress.getByName("127.0.0.1")));
+        cc.addContact(new Contact("Drosik", InetAddress.getByName("127.0.0.1")));*/
 
         //On passe le contrôleur principal au contrôleur de vues pour que les vues de chat y ait accès
         ViewController viewController = ViewController.getInstance();
--- src/network/NetworkInterface.java
+++ src/network/NetworkInterface.java
@@ -155,6 +155,7 @@ public class NetworkInterface {
             os.close();
         } catch (IOException e) {
             e.printStackTrace();
+
         }
     }
 
