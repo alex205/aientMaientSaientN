db2094279bdddad75f348618fff02a0b2d043b78
Toon <guilhem.cichocki@mail.com>
2017-04-24 10:28:59 +0200

    Premeirs tests

---
+++ src/Test/ContactTest.java
@@ -0,0 +1,20 @@
+package Test;
+
+import model.Contact;
+import org.junit.Test;
+
+import java.net.InetAddress;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by toon on 24/04/17.
+ */
+public class ContactTest {
+    @Test
+    public void getFullPseudo() throws Exception {
+        Contact test = new Contact("MSN", InetAddress.getByName("127.0.0.1"));
+        assert(test.getFullPseudo().equals("MSN@/127.0.0.1"));
+    }
+
+}
